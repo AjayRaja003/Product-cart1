050d2a7e047f01c1ea35a6f98e59f2e8db5c867f
AJAY <ajay.raja0903@gmail.com>
2024-03-22 14:40:46 +0530

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import React, { useState } from 'react';
-import Card from './components/Card';
-import Navi from './components/Navi';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from 'react';
+import "./App.css"
+import TaskManager from './components/TaskManager';
 
 const App = () => {
-  const [cartValue, setCartValue] = useState(0);
   return (
-    <>
-    <Navi cartValue={cartValue} />
-    <Header />
-    <section>
-      <div className="container">
-        <div className="row">
-          <Card setCartValue={setCartValue}></Card>
-        </div>
-      </div>
-    <Footer />
-    </section>
-  </>
-);
-}
-
-
+    <div>
+     <TaskManager />
+    </div>
+  );
+};
 
 export default App;
